e72d5db9c0c6c2dcbfd883902354e3cad72fd4df
lucavont <cavrettiluca@live.it>
2023-01-31 00:41:16 +0100

    Header Cart and Button Cart updated and finished
    Added useEffect and little animations on the header cart + the buttons in the cart now works

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,27 +1,51 @@
-import React from "react";
+import { useContext } from "react";
+
 import Modal from "../UI/Modal";
+import CartItem from "./CartItem";
 import classes from "./Cart.module.css";
+import CartContext from "../../store/cart-context";
 
 const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
+
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
+
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({ ...item, amount: 1 });
+  };
+
   const cartItems = (
     <ul className={classes["cart-items"]}>
-      {[{ id: "c1", name: "Sushi", amount: 2, price: 12.0 }].map((item) => (
-        <li>{item.name}</li>
+      {cartCtx.items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
       ))}
     </ul>
   );
+
   return (
     <Modal onClose={props.onClose}>
       {cartItems}
       <div className={classes.total}>
-        <span>Quantita Totale</span>
-        <span>35.60</span>
+        <span>Total Amount</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
         <button className={classes["button--alt"]} onClick={props.onClose}>
-          Chiudi
+          Close
         </button>
-        <button className={classes.button}>Ordina</button>
+        {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
   );
